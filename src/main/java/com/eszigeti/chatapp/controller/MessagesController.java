1b90a56be3e20a70539daa0e863cbb3c30132cdf
ErikSzigeti <erik.szigeti01@gmail.com>
2022-07-01 20:12:30 +0200

    Added request logging to controllers.

--- src/main/java/com/eszigeti/chatapp/controller/MessagesController.java
+++ src/main/java/com/eszigeti/chatapp/controller/MessagesController.java
@@ -3,6 +3,8 @@ package com.eszigeti.chatapp.controller;
 import com.eszigeti.chatapp.model.Message;
 import com.eszigeti.chatapp.service.MessageSenderService;
 import com.eszigeti.chatapp.service.MessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +12,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/messages")
 public class MessagesController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessagesController.class);
 
     private final MessageService messageService;
     private final MessageSenderService messageSenderService;
@@ -21,11 +25,13 @@ public class MessagesController {
 
     @PostMapping("/{userId}/send")
     public void sendMessage(@PathVariable long userId, @RequestParam long receiverId, @RequestBody Message message) {
+        logger.debug("Incoming request to Path /messages/" + userId + "/send Params:  " + receiverId + "  Request: " + message);
         messageSenderService.sendMessage(userId, receiverId, message);
     }
 
     @GetMapping("/{userId}")
     public List<Message> getAllMessagesBetweenUsers(@PathVariable long userId, @RequestParam long otherUserId) {
+        logger.debug("Incoming request to Path /messages/" + userId + "  Params: " + otherUserId);
         return messageService.getMessages(userId, otherUserId);
     }
 }
--- src/main/java/com/eszigeti/chatapp/controller/UserController.java
+++ src/main/java/com/eszigeti/chatapp/controller/UserController.java
@@ -2,11 +2,15 @@ package com.eszigeti.chatapp.controller;
 
 import com.eszigeti.chatapp.model.User;
 import com.eszigeti.chatapp.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService;
 
@@ -16,11 +20,13 @@ public class UserController {
 
     @PostMapping("/save")
     public User saveUser(@RequestBody User user) {
+        logger.debug("Incoming request to Path : /user/save  Request: " + user);
         return userService.saveUser(user);
     }
 
     @DeleteMapping("/delete/{userId}")
     public void deleteUser(@PathVariable long userId) {
+        logger.debug("Incoming request to Path: /delete/" + userId);
         userService.deleteById(userId);
     }
 
